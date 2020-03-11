a4cbf2fdcfc8a3dda0542155d705b95cf85bc23a
PrincetonPoh <58257346+PrincetonPoh@users.noreply.github.com>
2020-03-11 17:56:09 +0800

    Change to Button

--- 1D_Project/kivy_stuff/Dwdw.py
+++ 1D_Project/kivy_stuff/Dwdw.py
@@ -4,33 +4,15 @@ from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.widget import Widget
 from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.popup import Popup
 
-#get = GetList
-#lst = get.floor1()
-
-<<<<<<< HEAD
-class Widgets(Widget):
-    def btn(self):
-        show_popup()
-=======
-#def callback(instance):
-  #  string = str(instance)
-
- #   lst = get.string
-
-    #print('The button <%s> is being pressed' % instance.text)
-
-#floor1 = Button(text='Floor 1')
-#floor1.bind(on_press=callback)
-#floor2 = Button(text='Floor 2')
-#floor2.bind(on_press=callback)
->>>>>>> d5f0cdbe6b3d693556653947d8e437247c53bf8a
 
 class P(FloatLayout):
     pass
 
 class MainWindow(Screen):
-    pass
+    def btn(self):
+        show_popup()
 
 class SecondWindow(Screen):
     pass
