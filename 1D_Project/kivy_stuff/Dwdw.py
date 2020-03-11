d5f0cdbe6b3d693556653947d8e437247c53bf8a
tzavik <56147195+tzavik@users.noreply.github.com>
2020-03-11 17:18:27 +0800

    tried oop

--- 1D_Project/kivy_stuff/Dwdw.py
+++ 1D_Project/kivy_stuff/Dwdw.py
@@ -3,7 +3,20 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 
+#get = GetList
+#lst = get.floor1()
 
+#def callback(instance):
+  #  string = str(instance)
+
+ #   lst = get.string
+
+    #print('The button <%s> is being pressed' % instance.text)
+
+#floor1 = Button(text='Floor 1')
+#floor1.bind(on_press=callback)
+#floor2 = Button(text='Floor 2')
+#floor2.bind(on_press=callback)
 
 
 class MainWindow(Screen):
