51ab881d2304c16592d44bae51ae6c3a27d12d6e
PrincetonPoh <58257346+PrincetonPoh@users.noreply.github.com>
2020-03-17 22:08:23 +0800

    testing id
    i.e objectproperty

--- 1D_Project/kivy_stuff/Dwdw.py
+++ 1D_Project/kivy_stuff/Dwdw.py
@@ -5,12 +5,18 @@ from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.widget import Widget
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.popup import Popup
+'''class Color_btn(Widget):
+    def __init__(self, in_color):
+        self.btn.color = in_color'''
 
 
 class P(FloatLayout):
     pass
 
 class MainWindow(Screen):
+    '''def __init__(self, **kwargs):
+        super(MainWindow, self).__init__(**kwargs)
+        self.btn_1.background_color = in_color'''
     def btn(self):
         show_popup()
 
