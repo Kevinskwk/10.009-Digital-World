99954953233fcbe3371fa25be816667fd220694e
PrincetonPoh <58257346+PrincetonPoh@users.noreply.github.com>
2020-03-17 22:10:50 +0800

    objectproperty test

--- 1D_Project/kivy_stuff/Dwdw.py
+++ 1D_Project/kivy_stuff/Dwdw.py
@@ -5,6 +5,11 @@ from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.widget import Widget
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.popup import Popup
+from kivy.properties import ObjectProperty
+from kivy.uix.button import Button
+
+in_color = 1,0,0,1
+
 '''class Color_btn(Widget):
     def __init__(self, in_color):
         self.btn.color = in_color'''
@@ -17,8 +22,13 @@ class MainWindow(Screen):
     '''def __init__(self, **kwargs):
         super(MainWindow, self).__init__(**kwargs)
         self.btn_1.background_color = in_color'''
+
     def btn(self):
+        #self.btn_1.background_color = in_color
         show_popup()
+    
+    def update(self):
+        self.btn_1.background_color = in_color
 
 class SecondWindow(Screen):
     pass
