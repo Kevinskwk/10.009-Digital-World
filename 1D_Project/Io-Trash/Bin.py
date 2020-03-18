6dad4d8dd7040793d89cade3d24092bb11638bf8
kevinskwk <kevinskwk@gmail.com>
2020-03-18 22:45:20 +0800

    update Bin.py

--- 1D_Project/Io-Trash/Bin.py
+++ 1D_Project/Io-Trash/Bin.py
@@ -1,4 +1,5 @@
 import RPi.GPIO as GPIO
+from gpiozero import LED, Button, InputDevice, DistanceSensor
 from libdw import pyrebase
 from credential import *
 import time
@@ -9,40 +10,67 @@ db = firebase.database()
 user = auth.sign_in_with_email_and_password(email, password)
 
 class Bin:
-    def __init__(self, ID, us, water, smell):
+    def __init__(self, ID, db, user, echoPin=23, triggerPin=24, waterPin=18, smellPin=17, buttonPin=20, LEDPin=21):
         self.ID = str(ID)
-        self.sensors = {'us': bool(us),
-                      'water': bool(water),
-                      'smell': bool(smell)}
+        self.US = DistanceSensor(echo=echoPin, trigger=triggerPin)
+        self.water = InputDevice(waterPin)
+        self.smell = InputDevice(smellPin)
+        self.button = Button(buttonPin)
+        self.button.when_pressed = self.button_pressed
+        self.LED = LED(LEDPin)
+        self.sensors = {'us': False,
+                      'water': False,
+                      'smell': False}
         self.needClean = self.sensors['us'] or self.sensors['water'] or self.sensors['smell'] 
         self.cleaning = False
+        self.db = db
+        self.user = user
 
     def get_data(self):
         # get ultrasonic, water and smell sensor data
-        pass
+        USReading = self.US.distance
+        waterReading = not self.water.activate
+        smellReading = self.smell.activate
+        if USReading <= 20:
+            self.sensors['us'] = True
+        else:
+            self.sensors['us'] = False
+        self.sensors['water'] = waterReading
+        self.sensors['smell'] = smellReading
 
-    def update(self, db, user):
-        db.child('current_bin_states').child(self.ID).set(self.sensors, user['idToken'])
+        currentState = self.sensors['us'] or self.sensors['water'] or self.sensors['smell']
+        if currentState and not self.needClean:
+            self.log(1)
+        elif not currentState and self.needClean:
+            self.log(0)
 
-    def check_button(self):
+        self.needClean = currentState
+
+
+    def update(self):
+        # update date to firebase
+        self.db.child('current_bin_states').child(self.ID).set(self.sensors, self.user['idToken'])
+
+    def button_pressed(self):
         # check if the button is pressed, change the cleaning state accordingly
-        pass
+        self.cleaning = not self.cleaning
+        self.LED.on
 
-    def log(self, db, user, logType):
+    def log(self, logType):
+        # log record to firebase for analysis
         t = time.localtime(time.time())
         log = '{} {} {} {}'.format(t.tm_wday, t.tm_hour, t.tm_min, logType)
-        db.child('log').child(self.ID).push(log, user['idToken'])
+        self.db.child('log').child(self.ID).push(log, self.user['idToken'])
 
-bin = Bin(1, 0, 0, 0)
+bin = Bin(1, db, user)
 
 def loop():
     while True:
-        bin.check_button()
         if not bin.cleaning:
             t = time.localtime(time.time())
             if t.tm_sec <= 1:
                 bin.get_data()
-                bin.update(db, user)
+                bin.update()
                 
 
 
