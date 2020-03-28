6454d564ac8d89e2f156683c312f2626b764ba26
kevinskwk <kevinskwk@gmail.com>
2020-03-28 22:24:08 +0800

    updates

--- 1D_Project/Week9_boundaryfollower_thymio/smbrainreal.py
+++ 1D_Project/Week9_boundaryfollower_thymio/smbrainreal.py
@@ -6,7 +6,7 @@ from boxworld import thymio_world
 
 
 class MySMClass(sm.SM):
-    start_state = None
+    start_state = 0
 
     def get_next_values(self, state, inp):
         # These two lines is to stop the robot
@@ -28,8 +28,21 @@ class MySMClass(sm.SM):
             left = -1
             right = -1
         # print(left,right)
-        next_state = state
-        return next_state, io.Action(fv=0.1, rv=0.2)
+        
+        if left == 1 and right == 1:
+            if state == 0:
+                return 0, io.Action(fv=0.2, rv=0.0)
+            else:
+                return 3, io.Action(fv=0.05, rv=-0.2)
+
+        if left == 0 and right == 0:
+            return 1, io.Action(fv=0.05, rv = 0.2)
+
+        if left == 1 and right == 0:
+            return 2, io.Action(fv=0.2, rv=0)
+
+        if left == 0 and right == 1:
+            return 4, io.Action(fv=0, rv=0.2)
 
     #########################################
     # Don't modify the code below.
@@ -46,7 +59,7 @@ MySM = MySMClass()
 
 ############################
 
-m = ThymioSMReal(MySM, thymio_world, scale=2)
+m = ThymioSMReal(MySM, thymio_world)#, scale=2)
 try:
     m.start()
 except KeyboardInterrupt:
