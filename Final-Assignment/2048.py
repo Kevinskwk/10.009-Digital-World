79935ffd15a27394b480809e602d4c8533cf0f02
Kevinskwk <kevinskwk@gmail.com>
2020-04-11 22:47:22 +0800

    Initial commit of 2048

---
+++ Final-Assignment/2048.py
@@ -0,0 +1,289 @@
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.core.window import Window
+
+import numpy as np
+import time
+import random
+
+tile_color_map = {
+    2: (0, 1, 0, 0.8),
+    4: (0, 0.8, 0.2, 0.8),
+    8: (0, 0.6, 0.4, 0.8),
+    16: (0, 0.4, 0.6, 0.8),
+    32: (0, 0.2, 0.8, 0.8),
+    64: (0, 0, 1, 0.8),
+    128: (0.2, 0, 0.8, 0.8),
+    256: (0.4, 0, 0.6, 0.8),
+    512: (0.6, 0, 0.4, 0.8),
+    1024: (0.8, 0, 0.2, 0.8),
+    2048: (1, 0, 0, 0)
+}
+
+class Tile(Label):
+    '''
+    customised Tile class with property value
+    when changing value, change text and color at the same time
+    '''
+    def __init__(self, value=0, **kwargs):
+        Label.__init__(self, **kwargs)
+        self.font_size = 48
+        self.value = value
+
+    def set_value(self, value):
+        self._value = value
+        if value == 0:
+            self.text = ''
+        else:
+            self.text = str(value)
+        if value in tile_color_map:
+            self.color = tile_color_map.get(value)
+
+    def get_value(self):
+        return self._value
+
+    value = property(get_value, set_value)
+
+class Menu(Screen):
+    def __init__(self, **kwargs):
+        Screen.__init__(self, **kwargs)
+        self.layout = BoxLayout(orientation='vertical')
+        title = Label(text='2048', font_size=48)
+        startButton = Button(text='Start', on_press=self.start_game, font_size=48)
+        
+        self.layout.add_widget(title)
+        self.layout.add_widget(startButton)
+        self.add_widget(self.layout)
+    
+    def start_game(self, value):
+        self.manager.transition.direction = 'right'
+        self.manager.current = 'game'
+
+class Game(Screen):
+    def __init__(self, **kwargs):
+        Screen.__init__(self, **kwargs)
+        self.layout = BoxLayout(orientation='vertical')
+        self.top_bar = BoxLayout(orientation='horizontal', size_hint_y=0.2)
+        self.grid = GridLayout(cols=4, padding=2)
+        self.score = 0
+        self.over = False
+        self.touch_initial = (0, 0)
+        self.matrix = np.zeros((4, 4), np.int)
+        self.tiles = []       
+        for i in range(4):
+            for j in range(4):
+                if j == 0:
+                    self.tiles.append([])
+                self.tiles[i].append(Tile(value=0))
+                self.grid.add_widget(self.tiles[i][j])
+        
+        # top bar
+        restartButton = Button(text='Restart', on_press=self.restart, font_size=24)
+        quitButton = Button(text='Quit', on_press=self.quit, font_size=24)
+        self.scoreLabel = Label(text=str(self.score), font_size=24)
+        self.top_bar.add_widget(quitButton)
+        self.top_bar.add_widget(self.scoreLabel)
+        self.top_bar.add_widget(restartButton)
+        
+        # swipe control
+        self.grid.bind(on_touch_down=self._touch_down)
+        self.grid.bind(on_touch_up=self._touch_up)
+
+        # keyboard control
+        self._keyboard = Window.request_keyboard(self._keyboard_closed, self)
+        self._keyboard.bind(on_key_down=self._on_keyboard_down)   
+
+        self.layout.add_widget(self.top_bar)
+        self.layout.add_widget(self.grid)
+        self.add_widget(self.layout)
+
+        # initialize with two tiles
+        self.add_tile()
+        self.add_tile()
+
+    def _touch_down(self, instance, touch):
+        self.touch_initial = (touch.x, touch.y)
+
+    def _touch_up(self, instance, touch):
+        if not self.over:
+            dx = touch.x - self.touch_initial[0]
+            dy = touch.y - self.touch_initial[1]
+
+            if abs(dx) >= abs(dy):
+                if dx < -40:
+                    self.move(3) # left
+                elif dx > 40:
+                    self.move(2) # right
+            else:
+                if dy < -40:
+                    self.move(0) # down
+                elif dy > 40:
+                    self.move(1) # up
+        return True
+    
+    def _keyboard_closed(self):
+        self._keyboard.unbind(on_key_down=self._on_keyboard_down)
+        self._keyboard = None
+
+    def _on_keyboard_down(self, keyboard, keycode, text, modifiers):
+        if not self.over:
+            if keycode[1] == 'up':
+                self.move(1)
+            elif keycode[1] == 'down':
+                self.move(0)
+            elif keycode[1] == 'left':
+                self.move(3)
+            elif keycode[1] == 'right':
+                self.move(2)
+            return True
+
+    def add_tile(self):
+        '''randomly add one tile with number 2 or 4'''
+        choices = np.array([2, 4])
+        probabilities = np.array([0.9, 0.1])
+        val = np.random.choice(choices, 1, p=probabilities)[0]
+
+        empties = self.get_empty()
+        empty_index = np.random.choice(empties.shape[0])
+        empty = empties[empty_index]
+
+        self.matrix[empty[0], empty[1]] = val
+        self.over = self.is_over()
+        self.update()
+        
+    def get_empty(self):
+        '''retuen a 2d numpy array with locations of empty entries'''
+        return np.argwhere(self.matrix == 0)
+
+    def move(self, direction, trial=False):
+        '''
+        one move of the game.
+        direction: 0, 1, 2, 3 represent down, up, right, left
+        tiral: when True: trial mode, won't change the matrix
+        return: whether changed, score of the move
+        '''
+        changed = False
+        score = 0
+        shift_dir = (direction + 1) % 2
+
+        if direction <= 1:
+            # up or down, split matrix into columns
+            for y in range(4):
+                col = self.matrix[:,y]
+                (new_col, s) = self.shift(col, shift_dir)
+                score += s
+                if (new_col != col).any():
+                    changed = True
+                    if not trial:
+                        self.matrix[:, y] = new_col
+
+        else:
+            # left or right, split matrix into rows
+            for x in range(4):
+                row = self.matrix[x,:]
+                (new_row, s) = self.shift(row, shift_dir)
+                score += s
+                if (new_row != row).any():
+                    changed = True
+                    if not trial:
+                        self.matrix[x,:] = new_row
+
+        if not trial and changed:
+            self.score += score
+            self.add_tile()
+
+
+        return (changed, score) 
+        
+
+    def shift(self, row, direction):
+        '''
+        shift the row and combine the rows
+        direction: left if direction==0, right if direction==1
+        return:output row, score
+        '''
+        if direction:
+            row = np.flip(row)
+        # shift
+        shifted_row = np.zeros([4], np.int)
+        i = 0
+        for n in row:
+            if n != 0:
+                shifted_row[i] = n
+                i += 1   
+        # combine
+        score = 0
+        output = np.zeros([4], np.int)
+        output_index = 0
+        skip = False
+        for i in range(3):
+            if skip or shifted_row[i] == 0:
+                skip = False
+                continue
+            output[output_index] = shifted_row[i]
+            if shifted_row[i] == shifted_row[i+1]:
+                output[output_index] += shifted_row[i+1]
+                score += shifted_row[i] * 2
+                skip = True
+            output_index += 1
+        
+        if not skip:
+            output[output_index] = shifted_row[-1]
+
+        if direction:
+            output = np.flip(output)
+
+        return (output, score)     
+
+    def update(self):
+        '''update the labels'''
+        for i in range(4):
+            for j in range(4):
+                self.tiles[i][j].value = self.matrix[i, j]
+        if self.over:
+            self.scoreLabel.text = "Game Over\n {}".format(self.score)
+        else:
+            self.scoreLabel.text = str(self.score)
+
+    def is_over(self):
+        if self.get_empty().size != 0:
+            return False
+        else:
+            for dir in range(4):
+                if self.move(dir, trial=True)[0] == True:
+                    return False
+            return True
+        
+
+    def restart(self, value):
+        '''restart the game, bound to restart button'''
+        self.score = 0
+        self.over = False
+        self.matrix = np.zeros((4, 4), np.int)
+        for row in self.tiles:
+            for tile in row:
+                tile.value = 0
+
+        self.add_tile()
+        self.add_tile()
+
+    def quit(self, value):
+        self.manager.transition.direction = 'left'
+        self.manager.current = 'menu'
+
+
+class GameApp(App):
+    def build(self):
+        sm = ScreenManager()
+        menu = Menu(name='menu')
+        game = Game(name='game')
+        sm.add_widget(menu)
+        sm.add_widget(game)
+        sm.current = 'menu'
+        return sm
+
+GameApp().run()
---
+++ Final-Assignment/README.md
@@ -0,0 +1,17 @@
+# 10.009-The Digital World Final Assignment
+# 2048
+
+Author: Kevin Ma Yuchen
+
+## How to run:
+- install the libraries in `reqirements.txt` with command `pip3 install -r requirements.txt`
+    - Do it in a vertual environment if you prefer. Check [this](https://github.com/Kevinskwk/Misc/blob/master/Python/Packages_and_Environments.md) for python virtual environment setup guidance.
+
+- run 2048.py with command `python3 2048.py`
+
+## How to play:
+- With mouse: press and drag up/down/left/right
+- With keyboard: use up/down/left/right keys
+
+## TODO:
+- Add save game feature using pickle?
